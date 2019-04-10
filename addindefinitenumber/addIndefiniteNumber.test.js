e64eba392f46cc6ef9c1a73782728a9897753bcd
Ibrahim Joseph <josephibrahi@gmail.com>
2019-04-10 09:43:09 +0100

    Add function to sum indefinite number and return the value, and with his test file certifyt the function

---
+++ addindefinitenumber/addIndefiniteNumber.test.js
@@ -0,0 +1,16 @@
+const { addIndefiniteNumber } = require('./addIndefiniteNumber');
+
+test('should output sum of indefinit number', function(){
+    const sumIdefiniteNum = addIndefiniteNumber(4,5,2,6,8,9,0);
+    expect(sumIdefiniteNum).toBe(34);
+});
+
+test('Should output falsy', function(){
+    const sumIdefiniteNum = addIndefiniteNumber('54',8,3,0,12);
+    expect(sumIdefiniteNum).toBeFalsy();
+});
+
+test('Should return falsy when boolean is among the value', function(){
+    const sumIdefiniteNum = addIndefiniteNumber(true, false, -9, 'hdhd', '+');
+    expect(sumIdefiniteNum).toBeFalsy();
+});
